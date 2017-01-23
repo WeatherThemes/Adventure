7831094acef14e4df36d613e1d1fe7063feadde2
Spandan <dhungelspandan@gmail.com>
2017-01-23 13:59:39 +0545

    aLL files added

---
+++ header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<title>Adventure Bootstrap Template</title>
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--
+
+Template 2078 Adventure
+
+http://www.tooplate.com/view/2078-adventure
+
+-->
+	<!-- Bootstrap CSS
+   ================================================== -->
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css">
+
+	<!-- Animate CSS
+   ================================================== -->
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/animate.min.css">
+
+	<!-- Font Icons
+   ================================================== -->
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/et-line-font.css">
+
+	<!-- Nivo Lightbox CSS
+   ================================================== -->
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/nivo-lightbox.css">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/nivo_themes/default/default.css">
+
+	<!-- Owl Carousel CSS
+   ================================================== -->
+   	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/owl.theme.css">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/owl.carousel.css">
+
+	<!-- BxSlider CSS
+   ================================================== -->
+   	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/bxslider.css">
+
+   	<!-- Main CSS
+   	================================================== -->
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/style.css">
+
+	<!-- Google web font
+   ================================================== -->
+	<link href='https://fonts.googleapis.com/css?family=Raleway:700' rel='stylesheet' type='text/css'>
+	<?php wp_head(); ?>
+</head>
